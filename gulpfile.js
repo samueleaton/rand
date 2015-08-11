a4ff69aceeb4605cfa4de506be02ad3d4ad3fcb4
eaton11 <sameaton11@gmail.com>
2015-08-10 23:28:11 -0600

    adds ESLint to build process

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,22 @@ var gulp = require("gulp")
 var uglify = require("gulp-uglify");
 var rename = require("gulp-rename");
 var plumber = require("gulp-plumber");
+var eslint = require("gulp-eslint");
 
-gulp.task("default", function(){
-	gulp.src("rand.js")
+gulp.task("lint", function(){
+	return gulp.src("rand.js")
+	.pipe(plumber())
+	.pipe(eslint({"configFile":".eslintrc"}))
+	.pipe(eslint.format())
+	.pipe(eslint.failOnError());
+});
+
+gulp.task("uglify", ["lint"], function(){
+	return gulp.src("rand.js")
 	.pipe(plumber())
 	.pipe(uglify())
 	.pipe(rename("rand.min.js"))
-	.pipe(gulp.dest("./"))
+	.pipe(gulp.dest("./"));
 });
+
+gulp.task("default", ["uglify"]);
--- rand.js
+++ rand.js
@@ -1,45 +1,50 @@
-var rand = (function(){
+module.exports = (function() {
+	'use strict';
 
-	var specialChars = "#!?@$&_";
-
+	var specialChars = '#!?@$&_';
 	var charMap = {};
-	charMap.numeric = "1234567890";
-	charMap.alpha = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+	charMap.numeric = '1234567890';
+	charMap.alpha = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
 	charMap.alphaNumeric = charMap.alpha + charMap.numeric;
 	charMap.special = charMap.alphaNumeric + specialChars;
 
-	var rand = {
-
-		shuffle: function(string){
+	var _rand = {
+		shuffle: function(string) {
 			var type = typeof string;
-			string = (string+'').split('');
+
+			string = (string + '').split('');
 			for (var i = string.length; i > 0;) {
 				var random = parseInt(Math.random() * i);
 				var temp = string[--i];
+
 				string[i] = string[random];
 				string[random] = temp;
 			}
-
-			return (type === "number") ? +string.join('') : string.join('');
+			return type === 'number' ? +string.join('') : string.join('');
 		},
-		
-		string: function(length, specialChars){
+
+		string: function(length, useSpecialChars) {
+			var map = _rand.shuffle(charMap[ useSpecialChars ? 'special' : 'alphaNumeric' ]);
+			var randomString = '';
+
 			length = length || 16;
-			var map = rand.shuffle(charMap[( specialChars  ? "special" : "alphaNumeric" )]);
-			var randomString = "";
-			for(var i = 0; i < length; i++) randomString += map.charAt( Math.floor(Math.random() * map.length) );
+
+			for (var i = 0; i < length; i++) {
+				randomString += map.charAt(Math.floor(Math.random() * map.length));
+			}
 			return randomString;
 		},
 
-		range: function(min, max, excludeMax){
-			return Math.floor(Math.random() * (max - min + ( excludeMax ? 0 : 1 ) )) + min;
+		range: function(min, max, excludeMax) {
+			return Math.floor(Math.random() * (max - min + (excludeMax ? 0 : 1))) + min;
 		},
 
-		number: function(length){
-			length = (length || 1);
-			return rand.range( Math.pow(10,length-1), Math.pow(10,length), true) ;
+		number: function(length) {
+			length = length || 1;
+			return _rand.range(Math.pow(10, length - 1), Math.pow(10, length), true);
 		}
 	};
 
-	return rand;
+	return _rand;
 })();
